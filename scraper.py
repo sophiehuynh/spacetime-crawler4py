84d1360ae4f87d540861df1d61f2bb82fe510889
Steven Chen <stevenc9@circinus-30.ics.uci.edu>
2020-11-04 10:50:22 -0800

    subdomain

--- scraper.py
+++ scraper.py
@@ -12,15 +12,16 @@ def extract_next_links(url, resp,mostCommonWords):
     # Implementation requred.
     foundLinks = list()
     if (".ics.uci.edu" in url) or (".cs.uci.edu" in url) or (".informatics.uci.edu" in url) or (".stat.uci.edu" in url) or ("today.uci.edu/department/information_computer_sciences" in url):
-        print("----------------------------------------------------",url)
+        # print("----------------------------------------------------",url)
         if (resp.status >= 600 and resp.status <=606):
             print(resp.error)
         if (resp.status >=200 and resp.status <=599):
             if (resp.status >=400 and resp.status <=599):
-                print("400-599 STATUS CODE")
+                # print("400-599 STATUS CODE")
+                pass
             else:
                 if (resp.raw_response is not None):
-                    print("GOOD LINK")
+                    # print("GOOD LINK")
                     #Beautiful Soup Object
                     soup = BeautifulSoup(resp.raw_response.content, 'html.parser')
                     content = Token()                                   #our Token Object
@@ -30,18 +31,32 @@ def extract_next_links(url, resp,mostCommonWords):
                     if (contentLen >= 200 and contentLen <= 3000):      #filtering out webpages with too little info or too much(very large) info
                         content.computeWordFreq()                     
                         if(content.top3Freq/contentLen <= 0.5):         #assert top 3 words dont make up more than 50% of page text/content
-                            print("ADDING NEW LINKS")
+                            # print("ADDING NEW LINKS")
                             for link in soup.find_all('a'):
                                 if link.get('href') is not None:        # REMOVE FRAGMENT
                                     URL = urldefrag(link.get('href'))[0]
                                     if (".ics.uci.edu" in URL) or (".cs.uci.edu" in URL) or (".informatics.uci.edu" in URL) or (".stat.uci.edu" in URL) or ("today.uci.edu/department/information_computer_sciences" in URL):
                                         foundLinks.append(URL)
                                         ##### UPDATE INSTANCE VARIABLES
-                                        result = {key: mostCommonWords.get(key, 0) + content.tokenDict.get(key, 0) for key in set(mostCommonWords) | set(content.tokenDict)}
-                                        print(result)
-                                        raise TypeError
+                                        mostCommonWords = {key: mostCommonWords.get(key, 0) + content.tokenDict.get(key, 0) for key in set(mostCommonWords) | set(content.tokenDict)}
+
+
+
+
+
+
+
+                                        ###### CHECK SUBDOMAIN: If the prev 5 chars is not '//www', is subdomain & not part of query
+                                        if ('.ics.uci.edu' in URL) and (URL.split('.ics.uci.edu')[0][-5:] != '//www') and ('?' not in URL.split('.ics.uci.edu')[0]):
+                                            f = open('Subdomains.txt','a')
+                                            f.write(URL+"\n")
+                                            f.close()
+                                            # print("THIS MIGHT BE A SUBDOMAIN:::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::",URL)
+
+
                     else:
-                        print("LITTLEBIGLITTLEBIGLITTLEBIGLITTLEBIGLITTLEBIG ---------------")
+                        pass
+                        # print("LITTLEBIGLITTLEBIGLITTLEBIGLITTLEBIGLITTLEBIG ---------------")
     return foundLinks
     #add to a list of subdomains for ics.uci url and increment count for that subdomain
     #TO CHECK :if the link is valid and is not leading to a trap, a similar page,
