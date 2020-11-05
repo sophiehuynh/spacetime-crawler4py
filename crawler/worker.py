e7bc3680917269367281e58e476c898a752bd875
Sophie Le Huynh <sophielh@circinus-30.ics.uci.edu>
2020-11-04 21:21:18 -0800

    will never break

--- crawler/worker.py
+++ crawler/worker.py
@@ -22,30 +22,33 @@ class Worker(Thread):
     def run(self):
         count = 0
         while True:
-            tbd_url = self.frontier.get_tbd_url()
-            if not tbd_url:
-                self.logger.info("Frontier is empty. Stopping Crawler.")
-                break
-            resp = download(tbd_url, self.config, self.logger)
-            self.logger.info(
-                f"Downloaded {tbd_url}, status <{resp.status}>, "
-                f"using cache {self.config.cache_server}.")
-            scraped_urls = scraper(tbd_url, resp,self.mostCommonWords,self.icsSubDomains,self.longestPage,self.similarURLs)
-            ###CHECKING IF INSTANCE VARIABLES WORKING
+            try:
+                tbd_url = self.frontier.get_tbd_url()
+                if not tbd_url:
+                    self.logger.info("Frontier is empty. Stopping Crawler.")
+                    break
+                resp = download(tbd_url, self.config, self.logger)
+                self.logger.info(
+                    f"Downloaded {tbd_url}, status <{resp.status}>, "
+                    f"using cache {self.config.cache_server}.")
+                scraped_urls = scraper(tbd_url, resp,self.mostCommonWords,self.icsSubDomains,self.longestPage,self.similarURLs)
+                ###CHECKING IF INSTANCE VARIABLES WORKING
 
-            #IMPLEMENT
-            print("THE SIZE OF THE FRONTIERRLS IS NOW: ",len(self.frontier.to_be_downloaded))
-            print("THE SIZE OF THE DISCOVERED URLS IS NOW: ",len(self.discoveredURLs))
-            for scraped_url in scraped_urls:                                                    #for each scraped url, visit only if unvisited before
-                if (scraped_url not in self.discoveredURLs):                                       #and add to visitedurl set and frontier
-                    self.discoveredURLs.add(scraped_url)
-                    self.frontier.add_url(scraped_url)
-            self.frontier.mark_url_complete(tbd_url)
-            time.sleep(self.config.time_delay)
-            count += 1
-            print("\n\n",count,"\n\n")
-        #once out of while loop, that means crawler has stopped and now 
-            # if count == 100:
-            #     print()
-            #     break
+                #IMPLEMENT
+                print("THE SIZE OF THE FRONTIERRLS IS NOW: ",len(self.frontier.to_be_downloaded))
+                print("THE SIZE OF THE DISCOVERED URLS IS NOW: ",len(self.discoveredURLs))
+                for scraped_url in scraped_urls:                                                    #for each scraped url, visit only if unvisited before
+                    if (scraped_url not in self.discoveredURLs):                                       #and add to visitedurl set and frontier
+                        self.discoveredURLs.add(scraped_url)
+                        self.frontier.add_url(scraped_url)
+                self.frontier.mark_url_complete(tbd_url)
+                time.sleep(self.config.time_delay)
+                count += 1
+                print("\n\n",count,"\n\n")
+            #once out of while loop, that means crawler has stopped and now 
+                # if count == 100:
+                #     print()
+                #     break
+            except:
+                print("WOOOOOOPSYYYYY IT BROKEEEEEEEEEEEEEEEEEEEEEEE BUT ITS OKAYYYYYYYYYYYYYYYYYYYYYYY")
             
